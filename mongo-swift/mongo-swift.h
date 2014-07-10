5f2b417c38b01538b050edd554d6365b87b625e8
Luke Lovett <luke.lovett@mongodb.com>
2014-07-10 12:10:57 -0700

    Initial Commit

---
+++ mongo-swift/mongo-swift.h
@@ -0,0 +1,19 @@
+//
+//  mongo-swift.h
+//  mongo-swift
+//
+//  Created by Luke Lovett on 7/10/14.
+//  Copyright (c) 2014 WDEM. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for mongo-swift.
+FOUNDATION_EXPORT double mongo_swiftVersionNumber;
+
+//! Project version string for mongo-swift.
+FOUNDATION_EXPORT const unsigned char mongo_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mongo_swift/PublicHeader.h>
+
+

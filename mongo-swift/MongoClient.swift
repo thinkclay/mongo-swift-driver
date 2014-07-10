4504902b9cec5c6144784ef7e969126693e129dd
Luke Lovett <luke.lovett@mongodb.com>
2014-07-10 14:02:22 -0700

    Bundling libbson, libmongoc with framework. Beginning work on MongoClient.

--- mongo-swift/MongoClient.swift
+++ mongo-swift/MongoClient.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+// TODO: use real bson type
+
+
+class MongoClient {
+    var __client:COpaquePointer
+    
+    init(connectionString:String) {
+        var cConnectionString:CString = connectionString.bridgeToObjectiveC().UTF8String
+        self.__client = mongoc_client_new(cConnectionString)
+    }
+    
+    func getDatabase(name:String) {
+        // TODO
+    }
+    
+    func runCommand(command:String) {
+        // TODO: this doesn't actually take a string. It takes a Document.
+    }
+}
